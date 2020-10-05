fc22a0ebe6f72a5dddeb2c5612634c4c10dfe3e2
Poppycop <Poppycop@DESKTOP-9HT1C81>
2020-10-05 12:28:53 -0700

    First version - added comments to given code

---
+++ CSE 360 - Assignment 1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,35 @@
+/*
+ * AddingMachine adds and subtracts numbers from an integer called total and can return
+ * a history of changes as a string.
+ */
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  //returns total
+  public int getTotal () {
+    return 0;
+  }
+  
+  //adds to total
+  public void add (int value) {
+  }
+
+  //subtracts from total
+  public void subtract (int value) {
+  }
+
+  //keeps history of changes to total and shows it as string
+  public String toString () {
+    return "";
+  }
+
+  //clears memory
+  public void clear() {
+  }
+}
